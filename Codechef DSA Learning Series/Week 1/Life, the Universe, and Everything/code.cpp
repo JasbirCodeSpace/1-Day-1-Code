516e4985841902bbc8738d466c48add8aa217eb8
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-08-13 20:53:47 +0530

    Week 1: Life, the Universe, and Everything

---
+++ Codechef DSA Learning Series/Week 1/Life, the Universe, and Everything/code.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unsigned short int temp = 0;
+
+    scanf("%u", &temp);
+    while (temp != 42)
+    {
+        printf("%u\n", temp);
+        scanf("%u", &temp);
+    }
+    return 0;
+}
