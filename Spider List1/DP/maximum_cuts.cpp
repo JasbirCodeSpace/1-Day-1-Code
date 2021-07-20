b853841b80fe0c98d05aa2abeb5e6d8627950d50
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2021-07-20 23:44:27 +0530

    updated maximum cuts

--- Spider List1/DP/maximum_cuts.cpp
+++ Spider List1/DP/maximum_cuts.cpp
@@ -24,13 +24,15 @@ int maxCuts2(int n, int a, int b, int c){
     dp[0] = 0;
     for(int i=1; i<=n; i++){
         if(i-a>=0)
-            dp[i] = max(dp[i], dp[i-a]+1);
+            dp[i] = max(dp[i], dp[i-a]);
         if(i-b>=0)
-            dp[i] = max(dp[i], dp[i-b]+1);
+            dp[i] = max(dp[i], dp[i-b]);
         if(i-c>=0)
-            dp[i] = max(dp[i], dp[i-c]+1);
+            dp[i] = max(dp[i], dp[i-c]);
+        if(dp[i]!=-1)
+            dp[i]++;
     }
-    return dp[n];
+    return dp[n]==-1?0:dp[n];
 }
 int main(int argc, char const *argv[])
 {
